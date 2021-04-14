d14c9267ddb98c37f2e6b2f8b434b9c769dec5d0
Lumbago <gleblubiskin@gmail.com>
2021-04-14 18:26:38 +0300

    add CORS

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -17,90 +17,92 @@ router.post(
       .isLength({ min: 6 }).notEmpty()
   ],
   async (req, res) => {
-  try {
+    try {
 
-    //errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({
-        errors: errors.array(),
-        message: 'Некорректны данные при регистрации'
-      });
+      //errors
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'Некорректны данные при регистрации'
+        });
+      }
+
+      //data to frontend
+      const {login, email, password} = req.body;
+
+      //checking for user existence
+      const candidateLogin = await User.findOne({ login });
+      const candidateEmail = await User.findOne({ email });
+      if ((candidateLogin || candidateEmail) || (candidateLogin && candidateEmail)) {
+        return res.status(400).json({ message: 'Такой пользователь уже существует' });
+      }
+
+      //hashing
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = new User({ login, email, password: hashedPassword });
+
+      //saving user to mongodb
+      await user.save();
+
+      //confirmation of success
+      res.status(201).json({ message: 'Пользователь создан' });
+
+    } catch (e) {
+      res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' + `${e}` });
     }
-
-    //data to frontend
-    const {login, email, password} = req.body;
-
-    //checking for user existence
-    const candidateLogin = await User.findOne({ login });
-    const candidateEmail = await User.findOne({ email });
-    if ((candidateLogin || candidateEmail) || (candidateLogin && candidateEmail)) {
-      return res.status(400).json({ message: 'Такой пользователь уже существует' });
-    }
-
-    //hashing
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const user = new User({ login, email, password: hashedPassword });
-
-    //saving user to mongodb
-    await user.save();
-
-    //confirmation of success
-    res.status(201).json({ message: 'Пользователь создан' });
-
-  } catch (e) {
-    res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' });
   }
-});
+);
 
 // /api/auth/login
 router.post(
   '/login',
   [
-    check('login', 'Введите login').exists(),
-    check('password', 'Введите пароль').exists()
+    check('login', 'Введите login').exists().notEmpty(),
+    check('password', 'Введите пароль').exists().notEmpty()
   ],
   async (req, res) => {
-  try {
+    try {
 
-    //errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({
-        errors: errors.array(),
-        message: 'Некорректные данные при входе в систему'
-      });
+      //errors
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'Некорректные данные при входе в систему'
+        });
+      }
+
+      //data to frontend
+      const {login, password} = req.body;
+
+      //checking to existing user 
+      const user = await User.findOne({ login });
+      if (!user) {
+        return res.status(400).json({ message: 'Пользователь не найден' });
+      }
+
+      //checking password
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ message: 'Неверный пароль, попробуйте снова' + `${e}`});
+      }
+
+      //generation user token
+      const token = jwt.sign(
+        { userId: user.id },
+        config.get('jwtSecret'),
+        { expiresIn: '1h' }
+      );
+      
+      //dispatch to frontend data
+      res.json({ token, userId: user.id });
+
+    } catch (e) {
+      res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' });
     }
-
-    //data to frontend
-    const {login, password} = req.body;
-
-    //checking to existing user 
-    const user = await User.findOne({ login });
-    if (!user) {
-      return res.status(400).json({ message: 'Пользователь не найден' });
-    }
-
-    //checking password
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ message: 'Неверный пароль, попробуйте снова' });
-    }
-
-    //generation user token
-    const token = jwt.sign(
-      { userId: user.id },
-      config.get('jwtSecret'),
-      { expiresIn: '1h' }
-    );
-    
-    //dispatch to frontend data
-    res.json({ token, userId: user.id });
-
-  } catch (e) {
-    res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' });
   }
-});
+);
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json({ extended: true }));
 app.use('/api/auth', require('./routes/auth.routes'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Use CORS
+app.use(cors())
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,7 +24,7 @@ async function start () {
             useCreateIndex: true
         } );
 
-        app.listen(PORT, () => console.log(`Hi - ${PORT}`));
+        app.listen(PORT, () => console.log(`Hi - server started on port ${PORT}`));
     } catch ( e ) {
         console.log('Server error', e.message);
         process.exit(1);
