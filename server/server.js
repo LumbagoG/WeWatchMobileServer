6cf1c7b85641b81b4237f4615a49467b404b3559
Lumbago <gleblubiskin@gmail.com>
2021-04-14 14:24:55 +0300

    installing dependencies and create .gitignore

--- server/server.js
+++ server/server.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
-const path = require('path');
 
 const app = express();
 
 app.use(express.json({ extended: true }));
 app.use('/api/auth', require('./routes/auth.routes'));
 
-const PORT = config.get('port') || 5000;
-
-// app.use(express.static(path.join(__dirname, '../build')));
-
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../build/index.html'))
-// });
+const PORT = process.env.PORT || 5000;
 
 async function start () {
     try {
